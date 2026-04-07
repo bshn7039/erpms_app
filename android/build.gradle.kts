77ab3e53c4f2ecc181d391f2ceed7d72af038cda
bshn.p <bhushanpawar7039@gmail.com>
2026-04-07 21:33:56 +0530

    Add files via upload

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,8 +15,24 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
+}
+
+// Force all subprojects to use a consistent JVM target (Java 11)
+// This resolves the "Inconsistent JVM Target" error between Java (11) and Kotlin (17)
+subprojects {
+    tasks.withType<JavaCompile>().configureEach {
+        sourceCompatibility = JavaVersion.VERSION_11.toString()
+        targetCompatibility = JavaVersion.VERSION_11.toString()
+    }
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
